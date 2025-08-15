b814242e235016ea6a44a47c96920fbd1ffa214f
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-15 19:27:26 +0600

    problem3 solved

--- problem2.js
+++ problem2.js
@@ -1,10 +1,13 @@
-function onlyCharacter(str){
-// const capital = str.toUpperCase();
-// return capital;
-const capital = str;
-str = str[str];
-if(str
+function  onlyCharacter( str ) {
+    // let splitCharacter =[];
+    if(typeof(str)=== "string"){
+        let splitCharacter= str.split('');
+        return splitCharacter;
+        
 
+    }   
+    else{
+        return "Invalid";
+    }   
 }
-}
-console.log(onlyCharacter('h ell o wo r l d'));
+console.log(onlyCharacter(" h e llo wor ld"));
--- problem3.js
+++ problem3.js
@@ -29,7 +29,7 @@ function bestTeam(player1, player2) {
   const totalCardOfplayer1 = player1.foul + player1.cardY + player1.cardR;
   const totalCardOfplayer2 = player2.foul + player2.cardY + player2.cardR;
 
-  if (typeof player1 === "object" && typeof player2 === "object") {
+  if (typeof (player1) === "object" && typeof (player2) === "object") {
     if (totalCardOfplayer1 < totalCardOfplayer2) {
       return player1.name;
     } else if (totalCardOfplayer1 === totalCardOfplayer2) {
@@ -42,24 +42,39 @@ function bestTeam(player1, player2) {
   }
 }
 
-// console.log(bestTeam({name: "Brazil", foul: 5, cardY: 3, cardR: 0} , {name: "Argentina", foul: 7, cardY:0, cardR:0}))
-console.log(bestTeam(50, 70));
-console.log(bestTeam(["string"], ["string2"]));
+// // console.log(bestTeam({name: "Brazil", foul: 5, cardY: 3, cardR: 0} , {name: "Argentina", foul: 7, cardY:0, cardR:0}))
+// console.log(bestTeam(50, 70));
+// // console.log(bestTeam(["string"], ["string2"]));
 
-console.log(
-  bestTeam(
-    { name: "Germany", foul: 12, cardY: 0, cardR: 0 },
-    { name: "Sweden", foul: 7, cardY: 4, cardR: 1 }
-  )
-);
+// console.log(
+//   bestTeam(
+//     { name: "Germany", foul: 12, cardY: 0, cardR: 0 },
+//     { name: "Sweden", foul: 7, cardY: 4, cardR: 1 }
+//   )
+// );
 
-console.log(
-  bestTeam(
-    { name: "Germany", foul: 10, cardY: 1, cardR: 1 },
-    { name: "France", foul: 10, cardY: 2, cardR: 1 }
-  )
-);
+// console.log(
+//   bestTeam(
+//     { name: "Germany", foul: 10, cardY: 1, cardR: 1 },
+//     { name: "France", foul: 10, cardY: 2, cardR: 1 }
+//   )
+// );
 
-console.log(
-  bestTeam({ name: "Germany", foul: 10, cardY: 1, cardR: 1 }, "France")
-);
+// console.log(
+//   bestTeam({ name: "Germany", foul: 10, cardY: 1, cardR: 1 }, "France")
+// );
+
+console.log(bestTeam({ name: "Brazil", foul: 5, cardY: 1, cardR: 0 },
+{ name: "Argentina", foul: 7, cardY: 0, cardR: 0 }
+
+))
+console.log(bestTeam({ name: "Germany", foul: 12, cardY: 0, cardR: 0 },
+{ name: "Sweden", foul: 7, cardY: 4, cardR: 1 }))
+console.log(bestTeam({ name: "Germany", foul: 10, cardY: 1, cardR: 1 },
+{ name: "France", foul: 10, cardY: 2, cardR: 1 }))
+
+console.log(bestTeam({ name: "Germany", foul: 10, cardY: 1, cardR: 1 },"France"
+
+
+
+))
